e9f7366742e24d88224e9495dd64ea3c3adb5db2
CamrynBowWow <95950447+CamrynBowWow@users.noreply.github.com>
2026-07-14 13:57:05 +0200

    Conditions Done

---
+++ CSharpCourseBeginners/Conditions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpCourseBeginners
+{
+    public class Conditions
+    {
+        public static void Conditionslearning()
+        {
+            Console.WriteLine("Welcome to the ticket office. What is your age?");
+
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            // if
+            if (age < 18)
+            {
+                Console.WriteLine("You are a youth. Your ticket price is $5");
+            }
+
+            // else if
+            else if (age > 65 && age < 2000)
+            {
+                // senior prices
+                Console.WriteLine("You are a senior. Your ticket price is $8");
+
+            }
+
+            else if (age > 2000)
+            {
+                Console.WriteLine("You are too old to be here");
+            }
+
+            // else
+            else
+            {
+                // Adult prices
+                Console.WriteLine("You are an adult. Your ticket price is $10");
+
+            }
+        }
+    }
+}
---
+++ CSharpCourseBeginners/Variables.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpCourseBeginners
+{
+    public class Variables
+    {
+        public static void VariablesLearning()
+        {
+            Console.WriteLine("Hello. What is your name?");
+
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Hello " + name);
+
+            Console.WriteLine("Hello. Where are you from?");
+
+            string country = Console.ReadLine();
+
+            Console.WriteLine("I have heard great things about people from " + country);
+
+            Console.WriteLine("How old are you?");
+
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Your age is: " + age);
+
+            //// string = a piece of text
+            //string myName = "DevMan";
+
+            //// integer (int) = whole number, 1 2 3 4 5
+            //int myNumber = 3;
+
+            //// float = is a number with a decimal
+            //float myDecimalNumber = 1.5f;
+
+            //// bool = true or false
+            //bool myBool = false;
+        }
+    }
+}
