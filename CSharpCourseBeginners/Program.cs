cccaef44bf6defa8da18f1cf3f2d866498560382
CamrynBowWow <95950447+CamrynBowWow@users.noreply.github.com>
2026-07-14 12:00:20 +0200

    Variables Done

--- CSharpCourseBeginners/Program.cs
+++ CSharpCourseBeginners/Program.cs
@@ -10,7 +10,35 @@ namespace CSharpCourseBeginners
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("Hello. What is your name?");
+
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Hello " + name);
+
+            Console.WriteLine("Hello. Where are you from?");
+
+            string country = Console.ReadLine();
+
+            Console.WriteLine("I have heard great things about people from " + country);
+
+            Console.WriteLine("How old are you?");
+
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Your age is: " + age);
+
+            //// string = a piece of text
+            //string myName = "DevMan";
+
+            //// integer (int) = whole number, 1 2 3 4 5
+            //int myNumber = 3;
+
+            //// float = is a number with a decimal
+            //float myDecimalNumber = 1.5f;
+
+            //// bool = true or false
+            //bool myBool = false;
         }
     }
 }
